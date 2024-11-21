d1b1119ec4f97ac1b29eff9d1a2501d9c1a2535c
Your Name <yourgmail address>
2024-11-21 17:57:26 +0530

    Starting Point

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <h2>LandingPage</h2>
+  )
+}
+
+export default LandingPage
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProfilePage() {
+  return (
+    <h2>ProfilePage</h2>
+  )
+}
+
+export default ProfilePage
